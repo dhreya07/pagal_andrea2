18d81e6f7c823a836f86fe101bbb5c4fbc0ff41d
andrea pagal2 <pagaldrea@gmail.com>
2025-09-14 00:08:34 +0800

    com

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -1,65 +1,65 @@
 class UsersController extends Controller {
 
-    public function_construct()
+    public function __construct()
     {
-        parent::function_construct();
+        parent::__construct();
+        $this->call->model('UsersModel');
     }
+
     public function index(): void 
     {
-        $this->call->model('UsersModel');
-        $data['users']=$this->UsersModel->all();
-        $this->call->view('users/index',$data);
+        $data['users'] = $this->UsersModel->all();
+        $this->call->view('users/index', $data);
     }
 
-    function create():void
+    public function create(): void
     {
-        if($this->io->method()=='post')
-            {
-                $username = $this->io->post('username');
-                $email = $this->io->post('email');
-
-                $data=array('username' => $username, 'email' => $email);
-                if($this->UsersModel->insert($data))
-                    {
-                        redirect();
-                    }else
-                    {
-                        echo "error";
-                    }
-            }
-            else
-                {
-                    $this->call->view('users/create')
-                }
-    }
-}
-
-     function update($id): void
-     {
-       $user =$this-> UsersModel -> find($id);
-       if(|$user)
-       {
-          echo "User not found";
-          return;
-       }
-       if($this->io->method()=='post')
-        {
+        if ($this->io->method() == 'post') {
             $username = $this->io->post('username');
             $email = $this->io->post('email');
 
-            $data=array('username' => $username, 'email => $email');
-
-            if($this->UsersModel->update($id,$data))
-                {
-                    redirect();
-                }
-                else{
-                    echo "error updating";
-                }
+            $data = array('username' => $username, 'email' => $email);
+            if ($this->UsersModel->insert($data)) {
+                redirect();
+            } else {
+                echo "error";
+            }
+        } else {
+            $this->call->view('users/create');
         }
-        else
-            {
+    }
+
+    public function update($id): void
+    {
+        $user = $this->UsersModel->find($id);
+        if (!$user) {
+            echo "User not found";
+            return;
+        }
+
+        if ($this->io->method() == 'post') {
+            $username = $this->io->post('username');
+            $email = $this->io->post('email');
+
+            $data = array('username' => $username, 'email' => $email);
+
+            if ($this->UsersModel->update($id, $data)) {
+                redirect();
+            } else {
+                echo "error updating";
+            }
+        } else {
             $data['user'] = $user;
             $this->call->view('users/update', $data);
         }
-     }
+    }
+
+    public function delete($id): void
+    {
+        if ($this->UsersModel->delete($id)) {
+            redirect();
+        } else {
+            echo "Error deleting user";
+        }
+    }
+}
